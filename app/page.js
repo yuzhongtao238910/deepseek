4a28be13a2b64984aed26b668c4b9543d5e39295
apple <yzt238910@163.com>
2025-05-05 23:22:43 +0800

    end

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
               <>
                 <div className="flex items-center gap-3">
                   <Image src={assets.logo_icon} alt="logo_icon" className="h-16"></Image>
-                  <p className="text-2xl font-medium">Hi, I'm DeepSeek</p>
+                  <p className="text-2xl font-medium">Hi, I&lsquo;m DeepSeek</p>
                 </div>
                 <p className="text-sm mt-2"> How can I help you today?</p>
               </>
